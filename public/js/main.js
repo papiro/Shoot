4e16042cbd62ff02b1b22c0e7607c8d68c3bcd5a
papiro <pierre.pirault@outlook.com>
2017-12-13 16:17:13 -0500

    id's change to be easier on the brain, and event handlers now tied to inner content of each pie piece

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,15 @@
-var one = document.getElementsByClassName('pie1');
+const one = document.getElementById('rock');
+const two = document.getElementById('paper');
+const three = document.getElementById('scissors');
 
-one[0].addEventListener('click', function(event) {
+one.addEventListener('click', function(event) {
   alert('Rock has been chosen');
 });
 
-var two = document.getElementsByClassName('pie2');
-
-two[0].addEventListener('click', function(event) {
+two.addEventListener('click', function(event) {
   alert('Paper has been chosen');
 });
 
-var three = document.getElementsByClassName('pie3');
-
-three[0].addEventListener('click', function(event) {
+three.addEventListener('click', function(event) {
   alert('Scissors has been chosen');
 });
