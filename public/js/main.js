1bd67832e0c237c11c6a8cb32baf617abd34a9d6
piraultjd <piraultjd@vcu.edu>
2017-12-13 15:25:37 -0500

    updated js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 var one = document.getElementsByClassName('pie1');
 
-one.addEventListener('click', function(event) {
+one[0].addEventListener('click', function(event) {
   alert('Rock has been chosen');
+});
+
+var two = document.getElementsByClassName('pie2');
+
+two[0].addEventListener('click', function(event) {
+  alert('Paper has been chosen');
+});
+
+var three = document.getElementsByClassName('pie3');
+
+three[0].addEventListener('click', function(event) {
+  alert('Scissors has been chosen');
 });
