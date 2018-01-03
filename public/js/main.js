044d866cc8423e2129104615311b2b87e5c05caa
papiro <pierre.pirault@outlook.com>
2018-01-03 17:41:35 -0500

    - added query params to other.html to differentiate between human or   robot opponent - added AI logic for robot - added id hook for h2 counter

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,67 @@
-['rock', 'scissors', 'paper'].forEach(function(item) {
-  const elem = document.getElementById(item);
-  elem.addEventListener('click', function(event) {
-    alert(item.charAt(0).toUpperCase() + item.slice(1) + ' has been chosen');
+const moves = {
+  PAPER: {
+    ROCK: 1,
+    PAPER: -1,
+    SCISSORS: 0
+  },
+  SCISSORS: {
+    PAPER: 1,
+    SCISSORS: -1,
+    ROCK: 0
+  },
+  ROCK: {
+    SCISSORS: 1,
+    ROCK: -1,
+    PAPER: 0
+  }
+};
+
+function playRobot () {
+  ['rock', 'scissors', 'paper'].forEach(function(item) {
+    const elem = document.getElementById(item);
+    elem.addEventListener('click', function(event) {
+      const userMove = item.toUpperCase();
+      const robotMove = getRobotMove();
+      const outcome = moves[userMove][robotMove];
+
+      switch (outcome) {
+        case 0:
+          console.log(`YOU LOSE:::${userMove} vs ${robotMove}`);
+          break;
+        case 1:
+          console.log(`YOU WIN:::${userMove} vs ${robotMove}`);
+          break;
+        case -1:
+          console.log(`TIE:::${userMove} vs ${robotMove}`);
+          break;
+        default:
+          console.log('something is gravely wrong');
+          break;
+      }
+    });
   });
-});
+}
+
+// return "PAPER", "SCISSORS", or "ROCK"- randomly
+function getRobotMove () {
+  return Object.keys(moves)[Math.floor(Math.random()*3)];
+}
+
+function playHuman () {
+
+}
+
+switch(window.location.search.split('=')[1]) {
+  case 'robot':
+    playRobot();
+    break;
+  case 'human':
+    playHuman();
+    // do something else with human
+    break;
+  default:
+    console.error('Invalid query parameter:::' + window.location.search);
+    break;
+}
+
+
