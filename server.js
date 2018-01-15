9d01732e6fe76abbf7d153fe7bb8133f4a1bc56a
papiro <pierre.pirault@outlook.com>
2018-01-15 14:16:25 -0500

    major updates to server.js - content-type response header being set - mime-types added for svg & css

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const 
   http = require('http'),
-  fs = require('fs')
+  fs = require('fs'),
+  path = require('path')
 ,
-  indexPath = './public/index.html'
+  public = 'public',
+  indexPath = path.join(public, 'index.html')
 ;
 
 const server = http.createServer( (req, res) => {
+  console.log(req.url)
   if (req.url === '/') {
     const index = fs.createReadStream(indexPath)
     index.pipe(res)
-  } 
+  } else {
+    const resource = fs.createReadStream(path.join(public, req.url))
+    resource.on('error', err => {
+      console.error('there was an error!:::', err)
+      res.statusCode = 404
+      res.end()
+    })
+
+    let mimeType = 'text/plain'
+    switch (path.extname(req.url)) {
+      case '.svg':
+        mimeType = 'image/svg+xml'
+        break
+      case '.css':
+        mimeType = 'text/css'
+        break
+    }
+    res.setHeader('Content-Type', mimeType)
+    resource.pipe(res)
+  }
 })
 
 server.listen('5555', '127.0.0.1')
